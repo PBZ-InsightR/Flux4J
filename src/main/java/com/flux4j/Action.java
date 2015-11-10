f309be932d63111300f2a0ecac79d25b629b8966
PBZ-InsightR <pbezault@yahoo.fr>
2015-11-10 10:45:33 +0100

    code indentation and import optimization

--- src/main/java/com/flux4j/Action.java
+++ src/main/java/com/flux4j/Action.java
@@ -1,12 +1,9 @@
 package com.flux4j;
 
-import java.util.function.Function;
-
 public class Action {
-
     private String actionType;
 
-    Action(String actionType){
+    Action(String actionType) {
         this.actionType = actionType;
     }
 }
