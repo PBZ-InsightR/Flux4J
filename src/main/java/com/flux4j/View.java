88c021b740c2e9a9d36c0df73e5e76fed7749451
PBZ-InsightR <pbezault@yahoo.fr>
2015-11-10 11:41:41 +0100

    views - first version

--- src/main/java/com/flux4j/View.java
+++ src/main/java/com/flux4j/View.java
@@ -1,5 +1,11 @@
 package com.flux4j;
 
-public class View {
-    // TODO
+import java.util.Observable;
+import java.util.Observer;
+
+public class View implements Observer {
+    @Override
+    public void update(Observable o, Object arg) {
+
+    }
 }
